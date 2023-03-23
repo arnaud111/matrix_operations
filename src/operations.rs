82e9bb8643427dbeb45e979d2a4cc790e84ea8be
Arnaud <arnaudjourdain@orange.fr>
2023-03-23 10:22:53 +0100

    add dot

--- src/operations.rs
+++ src/operations.rs
@@ -13,6 +13,24 @@ impl<T: Default + std::marker::Copy> Matrix<T> {
     }
 }
 
+impl<T: Default + std::marker::Copy + std::ops::Mul + std::ops::AddAssign<<T as std::ops::Mul>::Output>> Matrix<T> {
+
+    pub fn dot(&self, other: &Matrix<T>) -> Result<Matrix<T>, Box<dyn std::error::Error>> {
+        if self.shape.1 != other.shape.0 {
+            return Err("Matrix shapes are not compatible for dot product".into());
+        }
+        let mut matrix = Matrix::default((self.shape.0, other.shape.1));
+        for i in 0..self.shape.0 {
+            for j in 0..other.shape.1 {
+                for k in 0..self.shape.1 {
+                    matrix[i][j] += self[i][k] * other[k][j];
+                }
+            }
+        }
+        Ok(matrix)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +46,27 @@ mod tests {
                 assert_eq!(m1[i][j], m2[i][j]);
             }
         }
+    }
+
+    #[test]
+    fn dot() {
+        let m1 = Matrix::from_2d_vec(vec![vec![1, 2, 3], vec![4, 5, 6]]).unwrap();
+        let m2 = Matrix::from_2d_vec(vec![vec![1, 4], vec![2, 5], vec![3, 6]]).unwrap();
+        let m3 = Matrix::from_2d_vec(vec![vec![14, 32], vec![32, 77]]).unwrap();
+        let m4 = m1.dot(&m2).unwrap();
+        assert_eq!(m3.shape, m4.shape);
+        for i in 0..m3.shape.0 {
+            for j in 0..m3.shape.1 {
+                assert_eq!(m3[i][j], m4[i][j]);
+            }
+        }
+    }
+
+    #[test]
+    fn dot_error() {
+        let m1 = Matrix::from_2d_vec(vec![vec![1, 2, 3], vec![4, 5, 6]]).unwrap();
+        let m2 = Matrix::from_2d_vec(vec![vec![1, 4], vec![2, 5]]).unwrap();
+        let m3 = m1.dot(&m2);
+        assert!(m3.is_err());
     }
 }
