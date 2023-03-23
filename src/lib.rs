bfb7c110864434857a6622c493ae0a1e5b6124bd
Arnaud <arnaudjourdain@orange.fr>
2023-03-23 16:26:36 +0100

    clean doc

--- src/lib.rs
+++ src/lib.rs
@@ -47,11 +47,10 @@
 //! - Apply a function to each element of a matrix (like multiplying by a scalar, or adding a constant)
 //! - Apply a function on each element of two matrices (like multiplying two matrices element by element)
 //!
-pub mod operations;
 
 use std::error::Error;
 use std::fmt::{Display, Formatter};
-use std::ops::{Index, IndexMut};
+use std::ops::{Add, AddAssign, Index, IndexMut, Mul, Sub};
 
 /// A matrix struct that can be used to perform matrix operations.
 pub struct Matrix<T> {
@@ -389,5 +388,298 @@ impl<T: Default + Copy> Matrix<T> {
     /// ```
     pub fn shape(&self) -> (usize, usize) {
         self.shape
+    }
+
+    /// Apply a function to each element of the matrix
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::new(data, shape).unwrap();
+    ///
+    /// let new_matrix = matrix.apply(|x| x * 2);
+    ///
+    /// assert_eq!(new_matrix[0][0], 2);
+    /// assert_eq!(new_matrix[0][1], 4);
+    /// assert_eq!(new_matrix[0][2], 6);
+    /// assert_eq!(new_matrix[1][0], 8);
+    /// assert_eq!(new_matrix[1][1], 10);
+    /// assert_eq!(new_matrix[1][2], 12);
+    /// ```
+    pub fn apply(&self, f: fn(T) -> T) -> Matrix<T> {
+        let mut matrix = Matrix::default(self.shape);
+        for i in 0..self.shape.0 {
+            for j in 0..self.shape.1 {
+                matrix[i][j] = f(self[i][j]);
+            }
+        }
+        matrix
+    }
+
+    /// Apply a function to each element of two matrices and return a new matrix
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape = (2, 3);
+    ///
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape).unwrap();
+    ///
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix2 = Matrix::new(data2, shape).unwrap();
+    ///
+    /// let new_matrix = matrix1.apply_other(&matrix2, |x, y| x + y).unwrap();
+    ///
+    /// assert_eq!(new_matrix[0][0], 2);
+    /// assert_eq!(new_matrix[0][1], 4);
+    /// assert_eq!(new_matrix[0][2], 6);
+    /// assert_eq!(new_matrix[1][0], 8);
+    /// assert_eq!(new_matrix[1][1], 10);
+    /// assert_eq!(new_matrix[1][2], 12);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the matrices are not the same shape, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape1 = (2, 3);
+    /// let shape2 = (3, 2);
+    ///
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape1).unwrap();
+    ///
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix2 = Matrix::new(data2, shape2).unwrap();
+    ///
+    /// let new_matrix = matrix1.apply_other(&matrix2, |x, y| x + y);
+    ///
+    /// assert!(new_matrix.is_err());
+    /// ```
+    pub fn apply_other(&self, other: &Matrix<T>, f: fn(T, T) -> T) -> Result<Matrix<T>, Box<dyn Error>> {
+        if self.shape != other.shape {
+            return Err("Matrix shapes are not compatible for addition".into());
+        }
+        let mut matrix = Matrix::default(self.shape);
+        for i in 0..self.shape.0 {
+            for j in 0..self.shape.1 {
+                matrix[i][j] = f(self[i][j], other[i][j]);
+            }
+        }
+        Ok(matrix)
+    }
+
+    /// Transpose the matrix
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::new(data, shape).unwrap();
+    ///
+    /// let new_matrix = matrix.transpose();
+    ///
+    /// assert_eq!(new_matrix[0][0], 1);
+    /// assert_eq!(new_matrix[0][1], 4);
+    /// assert_eq!(new_matrix[1][0], 2);
+    /// assert_eq!(new_matrix[1][1], 5);
+    /// assert_eq!(new_matrix[2][0], 3);
+    /// assert_eq!(new_matrix[2][1], 6);
+    /// assert_eq!(new_matrix.shape(), (3, 2));
+    /// ```
+    pub fn transpose(&self) -> Matrix<T> {
+        let mut matrix = Matrix::default((self.shape.1, self.shape.0));
+        for i in 0..self.shape.0 {
+            for j in 0..self.shape.1 {
+                matrix[j][i] = self[i][j];
+            }
+        }
+        matrix
+    }
+}
+
+impl<T: Default + Copy + Mul<Output = T> + AddAssign<<T as Mul>::Output>> Matrix<T> {
+
+    /// Multiply two matrices together
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape1 = (2, 3);
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape1).unwrap();
+    ///
+    /// let shape2 = (3, 2);
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix2 = Matrix::new(data2, shape2).unwrap();
+    ///
+    /// let new_matrix = matrix1.dot(&matrix2).unwrap();
+    ///
+    /// assert_eq!(new_matrix[0][0], 22);
+    /// assert_eq!(new_matrix[0][1], 28);
+    /// assert_eq!(new_matrix[1][0], 49);
+    /// assert_eq!(new_matrix[1][1], 64);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the matrices are not compatible for multiplication, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape1 = (2, 3);
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape1).unwrap();
+    ///
+    /// let shape2 = (2, 3);
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix2 = Matrix::new(data2, shape2).unwrap();
+    ///
+    /// let new_matrix = matrix1.dot(&matrix2);
+    ///
+    /// assert!(new_matrix.is_err());
+    /// ```
+    pub fn dot(&self, other: &Matrix<T>) -> Result<Matrix<T>, Box<dyn Error>> {
+        if self.shape.1 != other.shape.0 {
+            return Err("Matrix shapes are not compatible for dot product".into());
+        }
+        let mut matrix = Matrix::default((self.shape.0, other.shape.1));
+        for i in 0..self.shape.0 {
+            for j in 0..other.shape.1 {
+                for k in 0..self.shape.1 {
+                    matrix[i][j] += self[i][k] * other[k][j];
+                }
+            }
+        }
+        Ok(matrix)
+    }
+}
+
+impl<T: Default + Copy + Add<Output = T>> Matrix<T> {
+
+    /// Add two matrices together
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape = (2, 3);
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape).unwrap();
+    /// let matrix2 = Matrix::new(data2, shape).unwrap();
+    ///
+    /// let new_matrix = matrix1.add(&matrix2).unwrap();
+    ///
+    /// assert_eq!(new_matrix[0][0], 2);
+    /// assert_eq!(new_matrix[0][1], 4);
+    /// assert_eq!(new_matrix[0][2], 6);
+    /// assert_eq!(new_matrix[1][0], 8);
+    /// assert_eq!(new_matrix[1][1], 10);
+    /// assert_eq!(new_matrix[1][2], 12);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the matrices are not compatible for addition, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape1 = (2, 3);
+    /// let shape2 = (3, 2);
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape1).unwrap();
+    /// let matrix2 = Matrix::new(data2, shape2).unwrap();
+    ///
+    /// let new_matrix = matrix1.add(&matrix2);
+    ///
+    /// assert!(new_matrix.is_err());
+    /// ```
+    pub fn add(&self, other: &Matrix<T>) -> Result<Matrix<T>, Box<dyn Error>> {
+        if self.shape != other.shape {
+            return Err("Matrix shapes are not compatible for addition".into());
+        }
+        let mut matrix = Matrix::default(self.shape);
+        for i in 0..self.shape.0 {
+            for j in 0..self.shape.1 {
+                matrix[i][j] = self[i][j] + other[i][j];
+            }
+        }
+        Ok(matrix)
+    }
+}
+
+impl<T: Default + Copy + Sub<Output = T>> Matrix<T> {
+
+    /// Subtract two matrices together
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape = (2, 3);
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape).unwrap();
+    /// let matrix2 = Matrix::new(data2, shape).unwrap();
+    ///
+    /// let new_matrix = matrix1.sub(&matrix2).unwrap();
+    ///
+    /// assert_eq!(new_matrix[0][0], 0);
+    /// assert_eq!(new_matrix[0][1], 0);
+    /// assert_eq!(new_matrix[0][2], 0);
+    /// assert_eq!(new_matrix[1][0], 0);
+    /// assert_eq!(new_matrix[1][1], 0);
+    /// assert_eq!(new_matrix[1][2], 0);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the matrices are not compatible for subtraction, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape1 = (2, 3);
+    /// let shape2 = (3, 2);
+    /// let data1 = vec![1, 2, 3, 4, 5, 6];
+    /// let data2 = vec![1, 2, 3, 4, 5, 6];
+    /// let matrix1 = Matrix::new(data1, shape1).unwrap();
+    /// let matrix2 = Matrix::new(data2, shape2).unwrap();
+    ///
+    /// let new_matrix = matrix1.sub(&matrix2);
+    ///
+    /// assert!(new_matrix.is_err());
+    /// ```
+    pub fn sub(&self, other: &Matrix<T>) -> Result<Matrix<T>, Box<dyn Error>> {
+        if self.shape != other.shape {
+            return Err("Matrix shapes are not compatible for addition".into());
+        }
+        let mut matrix = Matrix::default(self.shape);
+        for i in 0..self.shape.0 {
+            for j in 0..self.shape.1 {
+                matrix[i][j] = self[i][j] - other[i][j];
+            }
+        }
+        Ok(matrix)
     }
 }
