0a5d80aed559d4ee3462c187e66691dea25d7c81
Arnaud <arnaudjourdain@orange.fr>
2023-03-23 11:04:53 +0100

    sub

--- src/operations.rs
+++ src/operations.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::ops::{Add, AddAssign, Mul};
+use std::ops::{Add, AddAssign, Mul, Sub};
 use crate::Matrix;
 
 impl<T: Default + Copy> Matrix<T> {
@@ -43,6 +43,22 @@ impl<T: Default + Copy + Add<Output = T>> Matrix<T> {
         for i in 0..self.shape.0 {
             for j in 0..self.shape.1 {
                 matrix[i][j] = self[i][j] + other[i][j];
+            }
+        }
+        Ok(matrix)
+    }
+}
+
+impl<T: Default + Copy + Sub<Output = T>> Matrix<T> {
+
+    pub fn sub(&self, other: &Matrix<T>) -> Result<Matrix<T>, Box<dyn Error>> {
+        if self.shape != other.shape {
+            return Err("Matrix shapes are not compatible for addition".into());
+        }
+        let mut matrix = Matrix::default(self.shape);
+        for i in 0..self.shape.0 {
+            for j in 0..self.shape.1 {
+                matrix[i][j] = self[i][j] - other[i][j];
             }
         }
         Ok(matrix)
@@ -107,6 +123,28 @@ mod tests {
         let m1 = Matrix::from_2d_vec(vec![vec![1, 2, 3], vec![4, 5, 6]]).unwrap();
         let m2 = Matrix::from_2d_vec(vec![vec![1, 4], vec![2, 5]]).unwrap();
         let m3 = m1.add(&m2);
+        assert!(m3.is_err());
+    }
+
+    #[test]
+    fn test_sub() {
+        let m1 = Matrix::from_2d_vec(vec![vec![1, 2, 3], vec![4, 5, 6]]).unwrap();
+        let m2 = Matrix::from_2d_vec(vec![vec![1, 4, 7], vec![2, 5, 8]]).unwrap();
+        let m3 = Matrix::from_2d_vec(vec![vec![0, -2, -4], vec![2, 0, -2]]).unwrap();
+        let m4 = m1.sub(&m2).unwrap();
+        assert_eq!(m3.shape, m4.shape);
+        for i in 0..m3.shape.0 {
+            for j in 0..m3.shape.1 {
+                assert_eq!(m3[i][j], m4[i][j]);
+            }
+        }
+    }
+
+    #[test]
+    fn test_sub_error() {
+        let m1 = Matrix::from_2d_vec(vec![vec![1, 2, 3], vec![4, 5, 6]]).unwrap();
+        let m2 = Matrix::from_2d_vec(vec![vec![1, 4], vec![2, 5]]).unwrap();
+        let m3 = m1.sub(&m2);
         assert!(m3.is_err());
     }
 }
