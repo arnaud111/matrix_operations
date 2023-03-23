23839e648c9e7c09b31c9ba18332d8cf093eec19
Arnaud <arnaudjourdain@orange.fr>
2023-03-23 15:56:56 +0100

    first doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,95 @@
+//! # Matrix_Operations
+//!
+//! Matrix_Operations is a Rust crate for performing matrix operations. It provides a set of functions for performing common matrix operations.
+//!
+//! ## Installation
+//!
+//! Add the following to your `Cargo.toml` file:
+//!
+//! ```toml
+//! [dependencies]
+//! matrix_operations = "0.1.0"
+//! ```
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use matrix_operations::Matrix;
+//!
+//! // Create a matrix from a 2 dimensional vector
+//! let mut m1 = Matrix::from_2d_vec(vec![vec![1, 2, 3], vec![4, 5, 6]]).unwrap();
+//!
+//! // Create a matrix from a 1 dimensional vector and a shape
+//! let m2 = Matrix::new(vec![1, 2, 3, 4, 5, 6], (3, 2)).unwrap();
+//!
+//! // Modify a matrix
+//! m1[0][0] = 10;
+//!
+//! // Multiply two matrices
+//! let m3 = m1.dot(&m2).unwrap();
+//!
+//! // Apply a function to each element of a matrix
+//! let m4 = m3.apply(|x| x * 2);
+//!
+//! // Apply a function to each element of two matrices
+//! let m5 = m3.apply_other(&m4, |x, y| x * y).unwrap();
+//!
+//! println!("{}", m5);
+//! ```
+//!
+//! ## Features
+//!
+//! - Create a matrix
+//! - Transpose a matrix
+//! - Multiply two matrices
+//! - Add two matrices
+//! - Subtract two matrices
+//! - Apply a function to each element of a matrix (like multiplying by a scalar, or adding a constant)
+//! - Apply a function on each element of two matrices (like multiplying two matrices element by element)
+//!
 pub mod operations;
 
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::ops::{Index, IndexMut};
 
+/// A matrix struct that can be used to perform matrix operations.
 pub struct Matrix<T> {
     data: Vec<T>,
     shape: (usize, usize),
 }
 
+/// Allows the matrix to be indexed as a 2-dimensional array
+///
+/// # Examples
+///
+/// To get specific elements of the matrix, use the `[row][col]` operator:
+/// ```
+/// use matrix_operations::Matrix;
+///
+/// let data = vec![1, 2, 3, 4, 5, 6];
+/// let shape = (2, 3);
+/// let matrix = Matrix::new(data, shape).unwrap();
+///
+/// assert_eq!(matrix[0][0], 1);
+/// assert_eq!(matrix[0][1], 2);
+/// assert_eq!(matrix[0][2], 3);
+/// assert_eq!(matrix[1][0], 4);
+/// assert_eq!(matrix[1][1], 5);
+/// assert_eq!(matrix[1][2], 6);
+/// ```
+///
+/// To get specific rows of the matrix, use the `[row]` operator:
+/// ```
+/// use matrix_operations::Matrix;
+///
+/// let data = vec![1, 2, 3, 4, 5, 6];
+/// let shape = (2, 3);
+/// let matrix = Matrix::new(data, shape).unwrap();
+///
+/// assert_eq!(matrix[0], vec![1, 2, 3]);
+/// assert_eq!(matrix[1], vec![4, 5, 6]);
+/// ```
 impl<T> Index<usize> for Matrix<T> {
     type Output = [T];
 
@@ -19,6 +100,22 @@ impl<T> Index<usize> for Matrix<T> {
     }
 }
 
+/// Allows the matrix to be modified as a 2-dimensional array
+///
+/// # Examples
+///
+/// To modify specific elements of the matrix, use the `[row][col]` operator:
+/// ```
+/// use matrix_operations::Matrix;
+///
+/// let mut data = vec![1, 2, 3, 4, 5, 6];
+/// let shape = (2, 3);
+/// let mut matrix = Matrix::new(data, shape).unwrap();
+///
+/// matrix[0][0] = 10;
+///
+/// assert_eq!(matrix[0][0], 10);
+/// ```
 impl<T> IndexMut<usize> for Matrix<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         let start = index * self.shape.1;
@@ -27,6 +124,27 @@ impl<T> IndexMut<usize> for Matrix<T> {
     }
 }
 
+/// Allows the matrix to be printed
+///
+/// # Examples
+///
+/// ```
+/// use matrix_operations::Matrix;
+///
+/// let data = vec![1, 2, 3, 4, 5, 6];
+/// let shape = (2, 3);
+/// let matrix = Matrix::new(data, shape).unwrap();
+///
+/// println!("{}", matrix);
+/// assert_eq!(format!("{}", matrix), "1 2 3 \n4 5 6 \n");
+/// ```
+///
+/// # Results
+///
+/// ```text
+/// 1 2 3
+/// 4 5 6
+/// ```
 impl<T> Display for Matrix<T> where T: Display {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut s = String::new();
@@ -42,6 +160,38 @@ impl<T> Display for Matrix<T> where T: Display {
 
 impl<T: Default + Copy> Matrix<T> {
 
+    /// Creates a new matrix from a 1 dimensional vector and a shape
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::new(data, shape).unwrap();
+    ///
+    /// assert_eq!(matrix[0][0], 1);
+    /// assert_eq!(matrix[0][1], 2);
+    /// assert_eq!(matrix[0][2], 3);
+    /// assert_eq!(matrix[1][0], 4);
+    /// assert_eq!(matrix[1][1], 5);
+    /// assert_eq!(matrix[1][2], 6);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the data length does not match the shape, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 2);
+    /// let matrix = Matrix::new(data, shape);
+    ///
+    /// assert!(matrix.is_err());
+    /// ```
     pub fn new(data: Vec<T>, shape: (usize, usize)) -> Result<Matrix<T>, Box<dyn Error>> {
         if data.len() != shape.0 * shape.1 {
             return Err("Data length does not match shape".into());
@@ -49,6 +199,23 @@ impl<T: Default + Copy> Matrix<T> {
         Ok(Matrix { data, shape })
     }
 
+    /// Creates a new matrix from a shape and initialises all elements with the default value
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape = (2, 3);
+    /// let matrix: Matrix<u32> = Matrix::default(shape);
+    ///
+    /// assert_eq!(matrix[0][0], 0);
+    /// assert_eq!(matrix[0][1], 0);
+    /// assert_eq!(matrix[0][2], 0);
+    /// assert_eq!(matrix[1][0], 0);
+    /// assert_eq!(matrix[1][1], 0);
+    /// assert_eq!(matrix[1][2], 0);
+    /// ```
     pub fn default(shape: (usize, usize)) -> Matrix<T> {
         Matrix {
             data: vec![T::default(); shape.0 * shape.1],
@@ -56,6 +223,23 @@ impl<T: Default + Copy> Matrix<T> {
         }
     }
 
+    /// Creates a new matrix from a shape and initialises all elements with a given value
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::initialised(shape, 10);
+    ///
+    /// assert_eq!(matrix[0][0], 10);
+    /// assert_eq!(matrix[0][1], 10);
+    /// assert_eq!(matrix[0][2], 10);
+    /// assert_eq!(matrix[1][0], 10);
+    /// assert_eq!(matrix[1][1], 10);
+    /// assert_eq!(matrix[1][2], 10);
+    /// ```
     pub fn initialised(shape: (usize, usize), value: T) -> Matrix<T> {
         Matrix {
             data: vec![value; shape.0 * shape.1],
@@ -63,6 +247,38 @@ impl<T: Default + Copy> Matrix<T> {
         }
     }
 
+    /// Creates a new matrix from a 1 dimensional slice and a shape
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::from_slice(&data, shape).unwrap();
+    ///
+    /// assert_eq!(matrix[0][0], 1);
+    /// assert_eq!(matrix[0][1], 2);
+    /// assert_eq!(matrix[0][2], 3);
+    /// assert_eq!(matrix[1][0], 4);
+    /// assert_eq!(matrix[1][1], 5);
+    /// assert_eq!(matrix[1][2], 6);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the data length does not match the shape, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 2);
+    /// let matrix = Matrix::from_slice(&data, shape);
+    ///
+    /// assert!(matrix.is_err());
+    /// ```
     pub fn from_slice(data: &[T], shape: (usize, usize)) -> Result<Matrix<T>, Box<dyn Error>> {
         if data.len() != shape.0 * shape.1 {
             return Err("Data length does not match shape".into());
@@ -73,6 +289,38 @@ impl<T: Default + Copy> Matrix<T> {
         })
     }
 
+    /// Creates a new matrix from a 1 dimensional vector and a shape
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::new(data, shape).unwrap();
+    ///
+    /// assert_eq!(matrix[0][0], 1);
+    /// assert_eq!(matrix[0][1], 2);
+    /// assert_eq!(matrix[0][2], 3);
+    /// assert_eq!(matrix[1][0], 4);
+    /// assert_eq!(matrix[1][1], 5);
+    /// assert_eq!(matrix[1][2], 6);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the data length does not match the shape, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 2);
+    /// let matrix = Matrix::new(data, shape);
+    ///
+    /// assert!(matrix.is_err());
+    /// ```
     pub fn from_vec(data: Vec<T>, shape: (usize, usize)) -> Result<Matrix<T>, Box<dyn Error>> {
         if data.len() != shape.0 * shape.1 {
             return Err("Data length does not match shape".into());
@@ -80,6 +328,36 @@ impl<T: Default + Copy> Matrix<T> {
         Ok(Matrix { data, shape })
     }
 
+    /// Creates a new matrix from a 2 dimensional vector
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![vec![1, 2, 3], vec![4, 5, 6]];
+    /// let matrix = Matrix::from_2d_vec(data).unwrap();
+    ///
+    /// assert_eq!(matrix[0][0], 1);
+    /// assert_eq!(matrix[0][1], 2);
+    /// assert_eq!(matrix[0][2], 3);
+    /// assert_eq!(matrix[1][0], 4);
+    /// assert_eq!(matrix[1][1], 5);
+    /// assert_eq!(matrix[1][2], 6);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// If the data vector is not rectangular, an error will be returned
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![vec![1, 2, 3], vec![4, 5]];
+    /// let matrix = Matrix::from_2d_vec(data);
+    ///
+    /// assert!(matrix.is_err());
+    /// ```
     pub fn from_2d_vec(data: Vec<Vec<T>>) -> Result<Matrix<T>, Box<dyn Error>> {
         let shape = (data.len(), data[0].len());
         for i in 1..shape.0 {
@@ -96,134 +374,20 @@ impl<T: Default + Copy> Matrix<T> {
         Ok(matrix)
     }
 
+    /// Returns the shape of the matrix
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix_operations::Matrix;
+    ///
+    /// let data = vec![1, 2, 3, 4, 5, 6];
+    /// let shape = (2, 3);
+    /// let matrix = Matrix::new(data, shape).unwrap();
+    ///
+    /// assert_eq!(matrix.shape(), (2, 3));
+    /// ```
     pub fn shape(&self) -> (usize, usize) {
         self.shape
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_new() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (2, 3);
-        let matrix = Matrix::new(data, shape).unwrap();
-        assert_eq!(matrix.data, vec![1, 2, 3, 4, 5, 6]);
-        assert_eq!(matrix.shape, (2, 3));
-    }
-
-    #[test]
-    fn test_new_error() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (3, 3);
-        let matrix = Matrix::new(data, shape);
-        assert!(matrix.is_err());
-    }
-
-    #[test]
-    fn test_default() {
-        let shape = (2, 3);
-        let matrix: Matrix<u32> = Matrix::default(shape);
-        assert_eq!(matrix.data, vec![0, 0, 0, 0, 0, 0]);
-        assert_eq!(matrix.shape, (2, 3));
-    }
-
-    #[test]
-    fn test_initialised() {
-        let shape = (2, 3);
-        let matrix: Matrix<u32> = Matrix::initialised(shape, 1);
-        assert_eq!(matrix.data, vec![1, 1, 1, 1, 1, 1]);
-        assert_eq!(matrix.shape, (2, 3));
-    }
-
-    #[test]
-    fn test_from_slice() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (2, 3);
-        let matrix = Matrix::from_slice(&data, shape).unwrap();
-        assert_eq!(matrix.data, vec![1, 2, 3, 4, 5, 6]);
-        assert_eq!(matrix.shape, (2, 3));
-    }
-
-    #[test]
-    fn test_from_slice_error() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (3, 3);
-        let matrix = Matrix::from_slice(&data, shape);
-        assert!(matrix.is_err());
-    }
-
-    #[test]
-    fn test_from_vec() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (2, 3);
-        let matrix = Matrix::from_vec(data, shape).unwrap();
-        assert_eq!(matrix.data, vec![1, 2, 3, 4, 5, 6]);
-        assert_eq!(matrix.shape, (2, 3));
-    }
-
-    #[test]
-    fn test_from_vec_error() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (3, 3);
-        let matrix = Matrix::from_vec(data, shape);
-        assert!(matrix.is_err());
-    }
-
-    #[test]
-    fn test_from_2d_vec() {
-        let data = vec![vec![1, 2, 3], vec![4, 5, 6]];
-        let matrix = Matrix::from_2d_vec(data).unwrap();
-        assert_eq!(matrix.data, vec![1, 2, 3, 4, 5, 6]);
-        assert_eq!(matrix.shape, (2, 3));
-    }
-
-    #[test]
-    fn test_from_2d_vec_error() {
-        let data = vec![vec![1, 2, 3], vec![4, 5]];
-        let matrix = Matrix::from_2d_vec(data);
-        assert!(matrix.is_err());
-    }
-
-    #[test]
-    fn test_index() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (2, 3);
-        let matrix = Matrix::new(data, shape).unwrap();
-        assert_eq!(matrix[0][0], 1);
-        assert_eq!(matrix[0][1], 2);
-        assert_eq!(matrix[0][2], 3);
-        assert_eq!(matrix[1][0], 4);
-        assert_eq!(matrix[1][1], 5);
-        assert_eq!(matrix[1][2], 6);
-    }
-
-    #[test]
-    fn test_index_mut() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (2, 3);
-        let mut matrix = Matrix::new(data, shape).unwrap();
-        matrix[0][0] -= 7;
-        matrix[0][1] += 8;
-        matrix[0][2] /= 9;
-        matrix[1][0] *= 10;
-        matrix[1][1] %= 11;
-        matrix[1][2] = 12;
-        assert_eq!(matrix[0][0], 1 - 7);
-        assert_eq!(matrix[0][1], 2 + 8);
-        assert_eq!(matrix[0][2], 3 / 9);
-        assert_eq!(matrix[1][0], 4 * 10);
-        assert_eq!(matrix[1][1], 5 % 11);
-        assert_eq!(matrix[1][2], 12);
-    }
-
-    #[test]
-    fn test_display() {
-        let data = vec![1, 2, 3, 4, 5, 6];
-        let shape = (2, 3);
-        let matrix = Matrix::new(data, shape).unwrap();
-        assert_eq!(format!("{}", matrix), "1 2 3 \n4 5 6 \n");
     }
 }
