f2b961019e6f8b4dd502819b7637738b75acec58
Arnaud <arnaudjourdain@orange.fr>
2023-03-24 19:04:00 +0100

    load_matrix_from_csv_excluding_columns

--- src/csv.rs
+++ src/csv.rs
@@ -132,20 +132,12 @@ pub fn load_matrix_from_csv_columns<T: Default + Copy + FromStr>(path: &str, sep
     let mut header = true;
     let mut index_columns: Vec<usize> = Vec::new();
     for line in reader.lines() {
+        let line_tmp = line?;
         if header {
             header = false;
-            let line_tmp = line?;
-            let split: Vec<&str> = line_tmp.split(separator).collect();
-            for i in 0..split.len() {
-                for j in 0..columns.len() {
-                    if split[i] == columns[j] {
-                        index_columns.push(i);
-                    }
-                }
-            }
+            index_columns = get_columns_headers_index(&columns, line_tmp, separator);
             continue;
         }
-        let line_tmp = line?;
         let split: Vec<&str> = line_tmp.split(separator).collect();
         let mut row: Vec<T> = Vec::new();
         for i in 0..split.len() {
@@ -159,6 +151,99 @@ pub fn load_matrix_from_csv_columns<T: Default + Copy + FromStr>(path: &str, sep
     }
 
     Ok(matrix)
+}
+
+/// Load a CSV file without gaven columns data to a Matrix.
+///
+/// # Examples
+///
+/// ```
+/// use matrix_operations::csv::load_matrix_from_csv_excluding_columns;
+///
+/// let path = "resources/test_read.csv";
+/// let separator = ",";
+/// let columns = vec!["col1".to_string()];
+///
+/// let matrix = load_matrix_from_csv_excluding_columns::<f32>(path, separator, columns).unwrap();
+///
+/// assert_eq!(matrix.shape(), (3, 1));
+/// assert_eq!(matrix[0], vec![2.0]);
+/// assert_eq!(matrix[1], vec![4.0]);
+/// assert_eq!(matrix[2], vec![6.0]);
+/// ```
+///
+/// If columns are not found, the function will ignore them.
+///
+/// ```
+/// use matrix_operations::csv::load_matrix_from_csv_excluding_columns;
+///
+/// let path = "resources/test_read.csv";
+/// let separator = ",";
+/// let columns = vec!["col3".to_string()];
+///
+/// let matrix = load_matrix_from_csv_excluding_columns::<f32>(path, separator, columns).unwrap();
+///
+/// assert_eq!(matrix.shape(), (3, 2));
+/// assert_eq!(matrix[0], vec![1.0, 2.0]);
+/// assert_eq!(matrix[1], vec![3.0, 4.0]);
+/// assert_eq!(matrix[2], vec![5.0, 6.0]);
+/// ```
+///
+/// # Panics
+///
+/// This function will panic if the file cannot be opened or if the separator is not found.
+///
+/// ```should_panic
+/// use matrix_operations::csv::load_matrix_from_csv_excluding_columns;
+///
+/// let path = "../resources/test_does_not_exist.csv";
+/// let separator = ",";
+/// let columns = vec!["col1".to_string()];
+///
+/// let matrix = load_matrix_from_csv_excluding_columns::<f32>(path, separator, columns).unwrap();
+/// ```
+pub fn load_matrix_from_csv_excluding_columns<T: Default + Copy + FromStr>(path: &str, separator: &str, columns: Vec<String>) -> Result<Matrix<T>, Box<dyn Error>> where T: Default + Copy + FromStr, <T as FromStr>::Err: 'static, <T as FromStr>::Err: Error {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let data: Vec<T> = Vec::new();
+    let mut matrix = Matrix::new(data, (0, 0))?;
+
+    let mut header = true;
+    let mut index_columns: Vec<usize> = Vec::new();
+    for line in reader.lines() {
+        let line_tmp = line?;
+        if header {
+            header = false;
+            index_columns = get_columns_headers_index(&columns, line_tmp, separator);
+            continue;
+        }
+        let split: Vec<&str> = line_tmp.split(separator).collect();
+        let mut row: Vec<T> = Vec::new();
+        for i in 0..split.len() {
+            if index_columns.contains(&i) {
+                continue;
+            }
+            let value = split[i].parse::<T>()?;
+            row.push(value);
+        }
+        matrix.add_row_from_vec(matrix.shape.0, row)?;
+    }
+
+    Ok(matrix)
+}
+
+fn get_columns_headers_index(columns: &Vec<String>, header: String, separator: &str) -> Vec<usize> {
+    let mut index_columns: Vec<usize> = Vec::new();
+    let split: Vec<&str> = header.split(separator).collect();
+    for i in 0..split.len() {
+        for j in 0..columns.len() {
+            if split[i] == columns[j] {
+                index_columns.push(i);
+            }
+        }
+    }
+    index_columns
 }
 
 /// Write a Matrix to a CSV file.
